10d882c089bf70cf699173a28db8df23ed246021
Matt Reynolds <mattreynolds@google.com>
2024-05-20 16:03:17 -0700

    Add robot functions slouch, stand, wave, dance

--- device_driver/serial.js
+++ device_driver/serial.js
@@ -1,22 +1,3 @@
-async function connectSerialPort() {
-    if (!navigator.serial) {
-        console.log('Web serial not available');
-    }
-    const port = await navigator.serial.requestPort();
-    try {
-        await port.open({ baudRate: 115200 });
-    } catch (error) {
-        console.log('Error opening serial port: ', error);
-        return null;
-    }
-    if (!port.readable) {
-        console.log('Serial port not readable');
-        return null;
-    }
-    console.log('Opened serial port');
-    return port;
-}
-
 async function writeLinesToSerialPort(port, lines) {
     const textEncoder = new TextEncoderStream();
     const writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
@@ -40,15 +21,123 @@ async function resetMotors(port) {
         'd = (180 - motor.absolute_position(port.B)) % 360',
         'if d > 180:',
         '  d -= 360',
-        'motor.run_for_degrees(port.B, d, 1000)'
+        'motor.run_for_degrees(port.B, d, 100)',
+        'd = (180 - motor.absolute_position(port.D)) % 360',
+        'if d > 180:',
+        '  d -= 360',
+        'motor.run_for_degrees(port.D, d, 100)'
     ].join('\n'));
 }
 
-async function requestSerialPort() {
-    const port = await connectSerialPort();
-    if (port) {
-        const CTRL_C = '\x03';  // Ctrl+C character
-        await writeLinesToSerialPort(port, [CTRL_C]);
-        await resetMotors(port);
-    }
+async function slouch(port) {
+    await runPythonOnRobot(port, [
+        'd = (60 - motor.absolute_position(port.B)) % 360',
+        'if d > 180:',
+        '  d -= 360',
+        'motor.run_for_degrees(port.B, d, 500)'
+    ].join('\n'));
 }
+
+async function stand(port) {
+    await runPythonOnRobot(port, [
+        'd = (180 - motor.absolute_position(port.B)) % 360',
+        'if d > 180:',
+        '  d -= 360',
+        'motor.run_for_degrees(port.B, d, 500)'
+    ].join('\n'));
+}
+
+async function wave(port) {
+    await runPythonOnRobot(port, 'motor.run_for_degrees(port.D, 360, 700)');
+}
+
+async function dance(port, duration, speed) {
+    await runPythonOnRobot(port, [
+        `motor.run(port.B, ${speed/2})`,
+        `motor.run(port.D, ${speed})`,
+        `time.sleep_ms(${duration})`,
+        'motor.stop(port.B)',
+        'motor.stop(port.D)',
+    ].join('\n'));
+}
+
+async function initRobotSerialPort(port) {
+    const CTRL_C = '\x03';  // Ctrl+C character
+    await writeLinesToSerialPort(port, [CTRL_C]);
+    await resetMotors(port);
+}
+
+async function connectSerialPort(port) {
+    if (port === window.robotSerialPort) {
+        console.log('Port already selected');
+        return;
+    }
+    try {
+        await port.open({ baudRate: 115200 });
+    } catch (error) {
+        console.log('Error opening serial port: ', error);
+        return;
+    }
+    console.log('Opened serial port');
+    if (!port.readable) {
+        console.log('Serial port not readable');
+        return;
+    }
+    if (window.robotSerialPort) {
+        await window.robotSerialPort.close();
+    }
+    window.robotSerialPort = port;
+    initRobotSerialPort(port);
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    window.robotSerialPort = null;
+
+    const ports = await navigator.serial.getPorts();
+    if (ports.length !== 0) {
+        connectSerialPort(ports[0]);
+    }
+
+    const connectButton = document.getElementById('btn-connect');
+    connectButton.onclick = async () => {
+        const port = await navigator.serial.requestPort();
+        if (port) {
+            connectSerialPort(port);
+        }
+    };
+
+
+    const slouchButton = document.getElementById('btn-slouch');
+    slouchButton.onclick = () => {
+        console.log('slouch');
+        if (window.robotSerialPort) {
+            slouch(window.robotSerialPort);
+        }
+    };
+
+    const standButton = document.getElementById('btn-stand');
+    standButton.onclick = () => {
+        console.log('stand');
+        if (window.robotSerialPort) {
+            stand(window.robotSerialPort);
+        }
+    };
+
+    const waveButton = document.getElementById('btn-wave');
+    waveButton.onclick = () => {
+        console.log('wave');
+        if (window.robotSerialPort) {
+            wave(window.robotSerialPort);
+        }
+    };
+
+    const danceButton = document.getElementById('btn-dance');
+    danceButton.onclick = () => {
+        console.log('dance');
+        if (window.robotSerialPort) {
+            await dance(window.robotSerialPort, 3000, 500);
+            setTimeout(() => { resetMotors(window.robotSerialPort); }, 3000);
+        }
+    };
+
+});
