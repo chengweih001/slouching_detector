f0f7ee28f12ff144aadd8f19afa6747d46d14a1d
Jack Hsieh <chengweih@google.com>
2024-05-21 23:04:05 -0700

    svg as external object

--- webrobot/animation.js
+++ webrobot/animation.js
@@ -2,15 +2,16 @@
 let body, head, arms, legs, hips, eye, eye2;
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    body = document.querySelector('#body')
-    head = document.querySelector('#head')
-    arms = document.querySelector('#arms')
-    legs = document.querySelector('#legs')
-    hips = document.querySelector('#hips')
+    const robotImage = document.querySelector('#robot-img'); 
+    body = robotImage.contentDocument.querySelector('#body')
+    head =  robotImage.contentDocument.querySelector('#head')
+    arms =  robotImage.contentDocument.querySelector('#arms')
+    legs =  robotImage.contentDocument.querySelector('#legs')
+    hips =  robotImage.contentDocument.querySelector('#hips')
 
-    eye = document.querySelector('#eye-1')
-    eye2 = document.querySelector('#eye-2')
-    vRobotSleep();
+    eye =  robotImage.contentDocument.querySelector('#eye-1')
+    eye2 =  robotImage.contentDocument.querySelector('#eye-2')
+    vRobotSleep();        
 });
 const vRobotMoveDown = () => {
     gsap.to([body, head, arms, legs, hips], 0.3, {y: '20px', yoyo: false, repeat: 0});
@@ -26,17 +27,11 @@ const vRobotBlink = () => {
     gsap.to([eye, eye2], 0.2, { scaleY: 0, yoyo: true, repeat: 1, transformOrigin: 'center'})
 } 
 
-const vRobotSleep = () => {
-    const eye = document.querySelector('#eye-1')
-    const eye2 = document.querySelector('#eye-2')
-    
+const vRobotSleep = () => {    
     gsap.to([eye, eye2], 0.2, { scaleY: 0.01, yoyo: false, repeat: 0, transformOrigin: 'center'})
   }
   
 const vRobotWake = () => {
-    const eye = document.querySelector('#eye-1')
-    const eye2 = document.querySelector('#eye-2')
-
     gsap.to([eye, eye2], 0.2, { scaleY: 1, yoyo: false, repeat: 0, transformOrigin: 'center'})
 }
 
